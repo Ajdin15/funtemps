5706e99a6ee7d2b429dadec5d0457e20507b7c60
Ajdin15 <ajdins@uia.no>
2023-02-17 19:23:14 +0100

    17.02.23

--- conv/conv.go
+++ conv/conv.go
@@ -1,54 +1,48 @@
 package conv
 
-import "testing"
+/*
+  I denne pakken skal alle konverteringfunksjonene
+  implementeres. Bruk engelsk.
+    FarhenheitToCelsius
+    CelsiusToFahrenheit
+    KelvinToFarhenheit
+    ...
+*/
 
-func TestFahrenheitToCelsius(t *testing.T) {
-    fahrenheit := 68.0
-    expectedCelsius := 20.0
-    actualCelsius := FahrenheitToCelsius(fahrenheit)
-
-    if actualCelsius != expectedCelsius {
-        t.Errorf("FahrenheitToCelsius(%f) = %f; expected %f", fahrenheit, actualCelsius, expectedCelsius)
-    }
+// Konverterer Farhenheit til Celsius
+func FarhenheitToCelsius(value float64) float64 {
+	Celsius = (Farhrenheit - 32)*(5/9)
+      // Her skal du implementere funksjonen
+	// Du skal ikke formattere float64 i denne funksjonen
+	// Gjør formattering i main.go med fmt.Printf eller
+	// lignende
+	return Celsius = 0
 }
 
-func FahrenheitToCelsius(fahrenheit float64) float64 {
-    return (fahrenheit - 32) * 5 / 9
+func FarhenheitToKelvin(value float64) float64 {
+       Kelvin = (Farhenheit - 32) * (5/9) + 32
+       return Kelvin = 0
 }
 
-
-func TestCelsiusToKelvin(t *testing.T) {
-    celsius := 25.0
-    expectedKelvin := 298.15
-    actualKelvin := CelsiusToKelvin(celsius)
-
-    if actualKelvin != expectedKelvin {
-        t.Errorf("CelsiusToKelvin(%f) = %f; expected %f", celsius, actualKelvin, expectedKelvin)
-    }
+func CelsiusToFarhenheit(value float64) float64 {
+       Fahrenheit = Celsius*(9/5) + 32
+       return Fahrenheit = 0
 }
 
-func CelsiusToKelvin(celsius float64) float64 {
-    return celsius + 273.15
+func CelsiusToKelvin(value float64) float64 {
+       Kelvin = Celsius + 273.15
+       return Kelvin = 0
 }
-func TestCelsiusToFahrenheit(t *testing.T) {
-    // Define some test cases
-    testCases := []struct {
-        celsius    float64
-        fahrenheit float64
-    }{
-        {0, 32},
-        {25, 77},
-        {100, 212},
-    }
 
-    // Iterate through each test case and check the result
-    for _, tc := range testCases {
-        // Convert Celsius to Fahrenheit
-        result := (tc.celsius * 9 / 5) + 32
-
-        // Check if the result matches the expected Fahrenheit value
-        if result != tc.fahrenheit {
-            t.Errorf("Celsius %f should be Fahrenheit %f, but got %f", tc.celsius, tc.fahrenheit, result)
-        }
-    }
+func KelvinToFarhenheit(value float64) float64 {
+        Farhrenheit = (Kelvin - 273.15)*(9/5) + 32
+        return Fahrenheit = 0
 }
+
+func KelvinToCelsius(value float64) float64 {
+         Celsius = Kelvin - 273.15
+        return Celsius = 0
+}
+
+// De andre konverteringsfunksjonene implementere her
+// ...
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Ajdin15/funtemps
+
+go 1.19
